60e379f182a685a335731a8819bc4d094470aa14
Hirofumi Seiyama <hseiyama@gmail.com>
2022-07-16 23:11:18 +0900

    プルアップ＋ボタン判定を追加

--- hello_serial/hello_serial.c
+++ hello_serial/hello_serial.c
@@ -7,10 +7,37 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define BUTTON_GPIO (2)
+#define LED_GPIO  PICO_DEFAULT_LED_PIN
+
 int main() {
+    unsigned char count = 0;
+    bool event;
+    char *message = NULL;
     stdio_init_all();
+    /* ボタン入力用のポート設定 */
+    gpio_init(BUTTON_GPIO);
+    gpio_set_dir(BUTTON_GPIO, GPIO_IN);
+    gpio_pull_up(BUTTON_GPIO);
+    //gpio_put(BUTTON_GPIO, true); /* 明示的な出力設定は不要だった */
+    /* LED出力用のポート設定 */
+    gpio_init(LED_GPIO);
+    gpio_set_dir(LED_GPIO, GPIO_OUT);
+
     while (true) {
-        printf("Hello, world!\n");
+        count++;
+        /* ボタン入力値を判定 */
+        event = gpio_get(BUTTON_GPIO);
+        if (!event) {
+            message = " + push";
+            count = 0;
+            gpio_put(LED_GPIO, true);
+        } else {
+            message = "";
+            gpio_put(LED_GPIO, false);
+        }
+
+        printf("Hello, world! (serial-%d)%s\n", count, message);
         sleep_ms(1000);
     }
     return 0;
