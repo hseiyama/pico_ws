4f50570a8af883712607e31f52ad466d32ebd961
Hirofumi Seiyama <hseiyama@gmail.com>
2022-07-31 15:57:06 +0900

    PWM出力先をGPIO6+7に変更

--- hello_pwm/hello_pwm.c
+++ hello_pwm/hello_pwm.c
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-// Output PWM signals on pins 0 and 1
+// Output PWM signals on pins 6 and 7
 
 #include "pico/stdlib.h"
 #include "hardware/pwm.h"
@@ -12,19 +12,19 @@
 int main() {
     /// \tag::setup_pwm[]
 
-    // Tell GPIO 0 and 1 they are allocated to the PWM
-    gpio_set_function(0, GPIO_FUNC_PWM);
-    gpio_set_function(1, GPIO_FUNC_PWM);
+    // Tell GPIO 6 and 7 they are allocated to the PWM
+    gpio_set_function(6, GPIO_FUNC_PWM);
+    gpio_set_function(7, GPIO_FUNC_PWM);
 
-    // Find out which PWM slice is connected to GPIO 0 (it's slice 0)
-    uint slice_num = pwm_gpio_to_slice_num(0);
+    // Find out which PWM slice is connected to GPIO 6 (it's slice 3)
+    uint slice_num = pwm_gpio_to_slice_num(6);
 
-    // Set period of 4 cycles (0 to 3 inclusive)
-    pwm_set_wrap(slice_num, 3);
-    // Set channel A output high for one cycle before dropping
-    pwm_set_chan_level(slice_num, PWM_CHAN_A, 1);
-    // Set initial B output high for three cycles before dropping
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, 3);
+    // Set period of 25000 cycles (0 to 24999 inclusive)
+    pwm_set_wrap(slice_num, 24999);
+    // Set channel A output high for 6250 cycle (1/4) before dropping
+    pwm_set_chan_level(slice_num, PWM_CHAN_A, 6250);
+    // Set initial B output high for 18750 cycles (3/4) before dropping
+    pwm_set_chan_level(slice_num, PWM_CHAN_B, 18750);
     // Set the PWM running
     pwm_set_enabled(slice_num, true);
     /// \end::setup_pwm[]
