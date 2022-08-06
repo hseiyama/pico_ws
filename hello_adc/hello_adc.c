dc4b7fad2ad3325b32b01748e13f3dda04c507ac
Hirofumi Seiyama <hseiyama@gmail.com>
2022-08-06 18:17:05 +0900

    ADC設定値を定数定義に変更

--- hello_adc/hello_adc.c
+++ hello_adc/hello_adc.c
@@ -9,6 +9,9 @@
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 
+#define ADC_GPIO_26 (26)    // GP26(ADC0)
+#define ADC_CHAN_ADC0 (0)   // ADC0(GP26)
+
 int main() {
     stdio_init_all();
     printf("ADC Example, measuring GPIO26\n");
@@ -16,9 +19,9 @@ int main() {
     adc_init();
 
     // Make sure GPIO is high-impedance, no pullups etc
-    adc_gpio_init(26);
-    // Select ADC input 0 (GPIO26)
-    adc_select_input(0);
+    adc_gpio_init(ADC_GPIO_26);
+    // Select ADC input ADC0 (GPIO26)
+    adc_select_input(ADC_CHAN_ADC0);
 
     while (1) {
         // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
