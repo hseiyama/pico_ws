cb96d74deb3e2ceea51759afd59346083d3028f6
Hirofumi Seiyama <hseiyama@gmail.com>
2022-08-19 23:19:18 +0900

    Revert "アラームの回数を確認"
    This reverts commit 8885e20871da8733649835ce7063fdad3b6efca9.

--- lowlevel/timer_lowlevel/timer_lowlevel.c
+++ lowlevel/timer_lowlevel/timer_lowlevel.c
@@ -30,12 +30,11 @@ static uint64_t get_time(void) {
 static volatile bool alarm_fired;
 
 static void alarm_irq(void) {
-    static uint8_t count = 0;
     // Clear the alarm irq
     hw_clear_bits(&timer_hw->intr, 1u << ALARM_NUM);
 
     // Assume alarm 0 has fired
-    printf("Alarm IRQ fired (%d)\n", count++);
+    printf("Alarm IRQ fired\n");
     alarm_fired = true;
 }
 
