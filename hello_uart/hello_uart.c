a9b3317a972b2be52db7a0331fbdf63d181e3aca
Hirofumi Seiyama <hseiyama@gmail.com>
2022-08-19 23:19:22 +0900

    Revert "UARTの出力時間を計測"
    This reverts commit 164bc3d85440d2300cff02f5a803f4a71cb96599.

--- hello_uart/hello_uart.c
+++ hello_uart/hello_uart.c
@@ -19,16 +19,7 @@
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
 
-#define LED_GPIO  (7)
-
-static u_int64_t u16_time;
-static u_int64_t u16_diff;
-
 int main() {
-    gpio_init(LED_GPIO);
-    gpio_set_dir(LED_GPIO, GPIO_OUT);
-    gpio_put(LED_GPIO, false);
-
     // Set up our UART with the required speed.
     uart_init(UART_ID, BAUD_RATE);
 
@@ -41,36 +32,13 @@ int main() {
     // In a default system, printf will also output via the default UART
 
     // Send out a character without any conversions
-    u16_time = time_us_64();
-    gpio_put(LED_GPIO, true);
     uart_putc_raw(UART_ID, 'A');
-    gpio_put(LED_GPIO, false);
-    u16_diff = time_us_64() - u16_time;
 
     // Send out a character but do CR/LF conversions
-    //uart_set_translate_crlf(UART_ID, true);
-    u16_time = time_us_64();
-    gpio_put(LED_GPIO, true);
     uart_putc(UART_ID, 'B');
-    gpio_put(LED_GPIO, false);
-    u16_diff = time_us_64() - u16_time;
-    //uart_putc(UART_ID, '\r');
-    //uart_putc(UART_ID, '\n');
 
     // Send out a string, with CR/LF conversions
-    u16_time = time_us_64();
-    gpio_put(LED_GPIO, true);
-    uart_puts(UART_ID, " Hello, UART!\r\n");
-    gpio_put(LED_GPIO, false);
-    u16_diff = time_us_64() - u16_time;
-
-    char ch = uart_getc(UART_ID);
-    uart_puts(UART_ID, "ch1=");
-    ch = uart_getc(UART_ID);
-    uart_putc_raw(UART_ID, ch);
-    uart_puts(UART_ID, "\r\nch2=");
-    ch = uart_getc(UART_ID);
-    uart_putc_raw(UART_ID, ch);
+    uart_puts(UART_ID, " Hello, UART!\n");
 }
 
 /// \end::hello_uart[]
