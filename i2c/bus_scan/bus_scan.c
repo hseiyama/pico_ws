f01b332bb4e75141f78bac1a4e13404759a3b292
Hirofumi Seiyama <hseiyama@gmail.com>
2022-09-18 22:41:23 +0900

    i2c eeprom 処理の適正化

--- i2c/bus_scan/bus_scan.c
+++ i2c/bus_scan/bus_scan.c
@@ -25,6 +25,9 @@
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
+#define PICO_I2C_SDA_PIN (20)
+#define PICO_I2C_SCL_PIN (21)
+
 // I2C reserves some addresses for special purposes. We exclude these from the scan.
 // These are any addresses of the form 000 0xxx or 111 1xxx
 bool reserved_addr(uint8_t addr) {
@@ -40,12 +43,12 @@ int main() {
 #else
     // This example will use I2C0 on the default SDA and SCL pins (GP4, GP5 on a Pico)
     i2c_init(i2c_default, 100 * 1000);
-    gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
-    gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
-    gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-    gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
+    gpio_set_function(PICO_I2C_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(PICO_I2C_SCL_PIN, GPIO_FUNC_I2C);
+    //gpio_pull_up(PICO_I2C_SDA_PIN);
+    //gpio_pull_up(PICO_I2C_SCL_PIN);
     // Make the I2C pins available to picotool
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(PICO_I2C_SDA_PIN, PICO_I2C_SCL_PIN, GPIO_FUNC_I2C));
 
     printf("\nI2C Bus Scan\n");
     printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
--- test/i2c_e2prom/i2c_e2prom.c
+++ test/i2c_e2prom/i2c_e2prom.c
@@ -6,8 +6,8 @@
 // This example will use I2C0 on GPIO8 (SDA) and GPIO9 (SCL) running at 400KHz.
 // Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
 #define I2C_PORT i2c0
-#define I2C_SDA 21
-#define I2C_SCL 22
+#define I2C_SDA (20)
+#define I2C_SCL (21)
 
 int main()
 {
@@ -22,6 +22,19 @@ int main()
     //gpio_pull_up(I2C_SCL);
 
     puts("Hello, world!");
+
+    // 書き込み操作コマンド（ページ書き込み）
+//    uint8_t au8a_tx_buff[5] = {0x00, 0x00, 0x99, 0x88, 0x77};
+    // 読み出し操作コマンド（順次読み出し）
+    uint8_t au8a_tx_buff[2] = {0x00, 0x00};
+    static uint8_t au8a_rx_buff[8];
+    static uint32_t u32a_tx;
+    static uint32_t u32a_rx;
+    // 書き込み操作
+//    u32a_tx = i2c_write_blocking(I2C_PORT, 0x54, au8a_tx_buff, sizeof(au8a_tx_buff), false);
+    // 読み出し操作
+    u32a_tx = i2c_write_blocking(I2C_PORT, 0x54, au8a_tx_buff, sizeof(au8a_tx_buff), true);
+    u32a_rx = i2c_read_blocking(I2C_PORT, 0x54, au8a_rx_buff, sizeof(au8a_rx_buff), false);
 
     return 0;
 }
